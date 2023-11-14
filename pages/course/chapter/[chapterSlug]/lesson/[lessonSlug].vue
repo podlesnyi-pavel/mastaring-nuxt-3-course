a3fbdad3ae0f39150d8dd159520631132d5ac5e4
Pavel Podlesnyi <podlesnyi.pavel.v@gmail.com>
2023-11-14 20:01:48 +0300

    add course data and composables folder

---
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -0,0 +1,26 @@
+<template>
+  <h2>Lesson</h2>
+  <p>This is a lesson</p>
+  <p>{{ chapter && chapter.title }}</p>
+  <p>{{ lesson && lesson.title }}</p>
+</template>
+
+<script lang="ts" setup>
+const course = useCourse();
+const route = useRoute();
+
+const chapter = computed(() => {
+  return course.chapters.find(
+    (chapter) => chapter.slug === route.params.chapterSlug
+  );
+});
+
+const lesson = computed(() => {
+  return (
+    chapter.value &&
+    chapter.value.lessons.find(
+      (lesson) => lesson.slug === route.params.lessonSlug
+    )
+  );
+});
+</script>
