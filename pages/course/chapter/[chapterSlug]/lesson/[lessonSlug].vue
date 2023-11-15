6934cebb5d35aa6e47d4b21b47d68cb959e7f8e9
Pavel Podlesnyi <podlesnyi.pavel.v@gmail.com>
2023-11-15 17:52:35 +0300

    update lessonSlug

--- pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -1,8 +1,27 @@
 <template>
-  <h2>Lesson</h2>
-  <p>This is a lesson</p>
-  <p>{{ chapter && chapter.title }}</p>
-  <p>{{ lesson && lesson.title }}</p>
+  <div>
+    <p class="mt-0 uppercase font-bold text-slate-400 mb-1">
+      Lesson {{ chapter && chapter.number }} - {{ lesson && lesson.number }}
+    </p>
+    <h2 class="my-0">{{ lesson && lesson.title }}</h2>
+    <div class="flex space-x-4 mt-2 mb-8">
+      <a
+        v-if="lesson && lesson.sourceUrl"
+        class="font-normal text-md text-gray-500"
+        :href="lesson && lesson.sourceUrl"
+      >
+        Download Source Code
+      </a>
+      <a
+        v-if="lesson && lesson.downloadUrl"
+        class="font-normal text-md text-gray-500"
+        :href="lesson && lesson.downloadUrl"
+      >
+        Download Video
+      </a>
+    </div>
+    <p>{{ lesson && lesson.text }}</p>
+  </div>
 </template>
 
 <script lang="ts" setup>
@@ -16,11 +35,8 @@ const chapter = computed(() => {
 });
 
 const lesson = computed(() => {
-  return (
-    chapter.value &&
-    chapter.value.lessons.find(
-      (lesson) => lesson.slug === route.params.lessonSlug
-    )
+  return chapter.value?.lessons.find(
+    (lesson) => lesson.slug === route.params.lessonSlug
   );
 });
 </script>
