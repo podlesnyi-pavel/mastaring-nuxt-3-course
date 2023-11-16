88868aadde34d04a2ba53fdd482778d552536909
Pavel Podlesnyi <podlesnyi.pavel.v@gmail.com>
2023-11-16 10:21:15 +0300

    add component videoplayer

--- pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -1,26 +1,27 @@
 <template>
   <div>
     <p class="mt-0 uppercase font-bold text-slate-400 mb-1">
-      Lesson {{ chapter && chapter.number }} - {{ lesson && lesson.number }}
+      Lesson {{ chapter?.number }} - {{ lesson?.number }}
     </p>
-    <h2 class="my-0">{{ lesson && lesson.title }}</h2>
+    <h2 class="my-0">{{ lesson?.title }}</h2>
     <div class="flex space-x-4 mt-2 mb-8">
       <a
-        v-if="lesson && lesson.sourceUrl"
+        v-if="lesson?.sourceUrl"
         class="font-normal text-md text-gray-500"
-        :href="lesson && lesson.sourceUrl"
+        :href="lesson?.sourceUrl"
       >
         Download Source Code
       </a>
       <a
-        v-if="lesson && lesson.downloadUrl"
+        v-if="lesson?.downloadUrl"
         class="font-normal text-md text-gray-500"
-        :href="lesson && lesson.downloadUrl"
+        :href="lesson?.downloadUrl"
       >
         Download Video
       </a>
     </div>
-    <p>{{ lesson && lesson.text }}</p>
+    <VideoPlayer v-if="lesson?.videoId" :video-id="lesson.videoId" />
+    <p>{{ lesson?.text }}</p>
   </div>
 </template>
 
